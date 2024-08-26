8a7e25b22cd41486d6e2cf31fa3e4fcb55cf6391
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-08-26 15:02:08 +0200

    feat: Update App component to use React Router for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import timeData from './assets/time.json';
+import Layout from './pages/layout';
+import Main from './pages/main';
+import NoPage from './pages/noPage';
+import StylePage from './pages/stylePage';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+	return (
+		<BrowserRouter basename="/NextPause2">
+			<AppRoutes />
+		</BrowserRouter>
+	);
 }
 
-export default App
+function AppRoutes() {
+	const navigate = useNavigate();
+	const location = useLocation();
+	const selectedSchool = localStorage.getItem('selectedSchool');
+
+	const schools = Object.keys(timeData).map((school) => `/${school}`);
+
+	const firstSchool = schools[0];
+
+	useEffect(() => {
+		if (!schools.includes(location.pathname)) {
+			if (selectedSchool && schools.includes(`/${selectedSchool}`)) {
+				navigate(`/${selectedSchool}`);
+			} else if (location.pathname === '/' && firstSchool) {
+				navigate(firstSchool);
+			}
+		}
+	}, [selectedSchool, navigate, location.pathname, firstSchool]);
+
+	return (
+		<Routes>
+			<Route path="/style" element={<StylePage />} />
+			<Route path="/" element={<Layout />}>
+				{schools.map((schoolPath) => (
+					<Route key={schoolPath} path={schoolPath} element={<Main />} />
+				))}
+			</Route>
+
+			<Route path="*" element={<NoPage />} />
+		</Routes>
+	);
+}
